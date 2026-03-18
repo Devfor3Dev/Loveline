989874c7208a92f69a078752cbe1dda814cd0855
Devfor3Dev <devfor3dev@gmail.com>
2026-03-18 02:46:32 +0000

    Update vite.config.js

--- ecomerce-project/vite.config.js
+++ ecomerce-project/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Le proxy n'est utile qu'en développement local
-})
-```
 
-### 2. Créer un fichier `.env.production` à la racine de ton projet React
-```
-VITE_API_URL=https://ton-backend.onrender.com
 })
+
