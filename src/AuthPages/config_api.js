716a167969fee2dfd065e8f00295ad3c469b3cd7
Justin <alexsteven6344@gmail.com>
2026-03-19 00:23:17 +0000

    complete profil-remastered

--- src/AuthPages/config_api.js
+++ src/AuthPages/config_api.js
@@ -1,7 +1,7 @@
 // config_api.js (ou l'endroit où tu stockes tes constantes)
 export const API = {
-    BASE_URL: 'https://loveline-cdfv.onrender.com', // L'URL de base de Django
-    REGISTER: '/users/',                   // Défini par UserController dans api.py
-    LOGIN: '/token/pair',                  // Défini par NinjaJWTDefaultController
-    REFRESH: '/token/refresh',             // Pour rafraîchir le token plus tard
+    BASE_URL: import.meta.env.VITE_API_URL + '/api',
+    REGISTER: '/users/',
+    LOGIN: '/token/pair',
+    REFRESH: '/token/refresh',
 };
