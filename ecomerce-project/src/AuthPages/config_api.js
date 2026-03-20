c815359c63fb963b701daf2a4cbb8828dcd00353
Devfor3Dev <devfor3dev@gmail.com>
2026-03-20 09:37:31 +0000

    Fix REGISTER endpoint URL format

--- ecomerce-project/src/AuthPages/config_api.js
+++ ecomerce-project/src/AuthPages/config_api.js
@@ -1,6 +1,6 @@
 export const API = {
     BASE_URL: import.meta.env.VITE_API_URL + '/api',
-    REGISTER: '/users',
+    REGISTER: '/users/',
     LOGIN: '/token/pair',
     REFRESH: '/token/refresh',
 };
