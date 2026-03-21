2b56d1e573245c326dd2e7f8852a0e4d80a6dca4
Manuem-dev <yemoletrader@gmail.com>
2026-03-21 22:57:21 +0000

    Delete src directory

--- src/AuthPages/config_api.js
+++
@@ -1,7 +0,0 @@
-// config_api.js (ou l'endroit où tu stockes tes constantes)
-export const API = {
-    BASE_URL: import.meta.env.VITE_API_URL + '/api',
-    REGISTER: '/users',
-    LOGIN: '/token/pair',
-    REFRESH: '/token/refresh',
-};
